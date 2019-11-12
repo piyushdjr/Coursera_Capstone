f7f8b20c2ccca008fcabe5ac2cf47682f8a06faa
piyushdjr <57670560+piyushdjr@users.noreply.github.com>
2019-11-12 22:57:12 +0530

    Add files via upload

---
+++ cappy.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Coursera Capstone Project
+# This is to check the introduction given to us about GIT HUB
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
